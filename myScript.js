c9109b5f9c7a209b4ec94582fa626cd953c2def1
ShanchaiZ <shanchai.zahid@gmail.com>
2022-09-27 18:33:23 -0400

    added an eventlistener button to 1 click = 1 color

--- myScript.js
+++ myScript.js
@@ -3,5 +3,11 @@
 const button = document.querySelector("#button");
 
 button.addEventListener("click", function() {
-    alert("hello");
+    document.body.style.backgroundColor = "blue"
 })
+
+
+
+// 1. Create a button in the HTML markup!
+// 2. Find the button in Js using document.queryselector 
+// 3. Add an addEventlistner with one click to a one color
