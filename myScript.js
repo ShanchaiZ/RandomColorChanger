6e440166f59b24348f1547d6e34524dd9b2f235a
ShanchaiZ <shanchai.zahid@gmail.com>
2022-09-27 18:38:20 -0400

    added randomization of rgb color code

--- myScript.js
+++ myScript.js
@@ -4,6 +4,9 @@ const button = document.querySelector("#button");
 
 button.addEventListener("click", function() {
     document.body.style.backgroundColor = "blue"
+    const r = Math.floor(Math.random() * 255); //randomized color of red
+    const g = Math.floor(Math.random() * 255); //randomized color of green
+    const b = Math.floor(Math.random() * 255); //randomized color of blue
 })
 
 
@@ -11,3 +14,4 @@ button.addEventListener("click", function() {
 // 1. Create a button in the HTML markup!
 // 2. Find the button in Js using document.queryselector 
 // 3. Add an addEventlistner with one click to a one color
+// 4. Randomize color using RGB code. RGB color code (0-255, 0-255, 0-255) 
