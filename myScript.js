65e787fc96c84a91c6c7fcb6b10012ff7f73e5c7
ShanchaiZ <shanchai.zahid@gmail.com>
2022-09-27 18:46:57 -0400

    newColor will be concatination of rbg random values used in string template. Then displayed as body of document

--- myScript.js
+++ myScript.js
@@ -1,17 +1,20 @@
 // RANDOM COLOR CHANGER:
 
 const button = document.querySelector("#button");
-
-button.addEventListener("click", function() {
-    document.body.style.backgroundColor = "blue"
-    const r = Math.floor(Math.random() * 255); //randomized color of red
-    const g = Math.floor(Math.random() * 255); //randomized color of green
-    const b = Math.floor(Math.random() * 255); //randomized color of blue
+button.addEventListener("click", function () {
+    const r = Math.floor(Math.random() * 255); //randomized color value of red
+    const g = Math.floor(Math.random() * 255); //randomized color value of green
+    const b = Math.floor(Math.random() * 255); //randomized color value of blue
+    const newColor = `rgb(${r}, ${g}, ${b})`; // new color will the concatination of r,g,b
+    document.body.style.backgroundColor = newColor; //and then will be displayed as the body 
 })
 
 
 
+//Pseudocode:
 // 1. Create a button in the HTML markup!
-// 2. Find the button in Js using document.queryselector 
+// 2. Find the button in Js using document.queryselector
 // 3. Add an addEventlistner with one click to a one color
-// 4. Randomize color using RGB code. RGB color code (0-255, 0-255, 0-255) 
+// 4. Randomize color using RGB code. RGB color code (0-255, 0-255, 0-255) using Math.floor(Math.random() * 255 different shades)
+// 5. Place the r,g,b variables in a string template
+// 6. then display the newcolor as the body of the document 
